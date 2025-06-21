a07f51f582470c67bed020985bf487fce3f570de
Vimarsh Chaturvedi <vimarsh.c@gmail.com>
2025-06-21 16:28:09 +0200

    RequestEncodingMixing I

--- requests/models.py
+++ requests/models.py
@@ -7,9 +7,60 @@
 
 
 class RequestEncodingMixin:
-    pass
+    @property
+    def path_url(self):
+        """
+        Build the path URL to use.
+        """
+
+        url = []
+        p = urlsplit(self.url)
+
+        path = p.path
+        if not path:
+            path = "/"
+        
+        url.append(path)
+
+        query = p.query
+        if query:
+            url.append("?")
+            url.append(query)
+
+        return "".join(url)
+    
+    @staticmethod
+    def _encode_params(data):
+        """
+        Encode parameters in a peice of data 
+
+        Will sucessfully encode parameters when passed as a dict or a list of 
+        2-tuples. Order is retained if data is a list of 2 tuples but arbitrary
+        if parameters are are supplied as a dict. 
+        """
 
 
+        if isinstance(data,(str,bytes)):
+            return data 
+        elif hasattr(data, "read"):
+            return data 
+        elif hasattr(data, "__iter__"):
+            result = []
+            for k,vs in to_key_val_list(data):
+                if isinstance(vs,basestring) or not hasattr(vs, "__iter__"):
+                    vs = [vs]
+                for v in vs:
+                    if v is not None:
+                        result.append(
+                            (
+                                k.encode("utf-8") if isinstance(k, str) else k,
+                                v.encode("utf-8") if isinstance(v, str) else v,
+                            )
+                        )
+        else:
+            return data
+    
+    
 
 class RequestHooksMixin:
     def register_hook(self,event,hook):
@@ -37,7 +88,7 @@ class RequestHooksMixin:
                 return False 
         
 
-class Request:
+class Request(RequestHooksMixin):
     """
 
     A user-created :class: `Request <Requests> object. 
